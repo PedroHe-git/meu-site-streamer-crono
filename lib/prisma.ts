3cd6814c464eed3ed844dcdd78f3956587c49e06
PedroHe-git <pedrohenmessias@gmail.com>
2026-01-01 19:46:00 -0300

    troca banco de dados supabase

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,35 +1,15 @@
-import { PrismaClient } from '@prisma/client'
-import { Pool, neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import ws from 'ws'
-
-neonConfig.webSocketConstructor = ws
-
-const connectionString = `${process.env.DATABASE_URL}`
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
-const createPrismaClient = () => {
-  // 👇 A MÁGICA ESTÁ AQUI: idleTimeoutMillis
-  const pool = new Pool({ 
-    connectionString,
-    max: 1, // Limita a 1 conexão por container para não saturar
-    idleTimeoutMillis: 1000, // Fecha conexão após 1s sem uso (Isso permite o banco dormir)
-    connectionTimeoutMillis: 5000, 
-  })
-  
-  const adapter = new PrismaNeon(pool)
-  
-  return new PrismaClient({ 
-    adapter,
-    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
-  })
-}
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+  });
 
-export const prisma = globalForPrisma.prisma ?? createPrismaClient()
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
+export default prisma;
